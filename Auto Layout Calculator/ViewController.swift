f90a602fc546de6f83842c9e222558a5d0ed5de8
Larry Nickerson <lnickers@austin.rr.com>
2017-11-29 17:57:57 -0600

    Initial Commit

---
+++ Auto Layout Calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Auto Layout Calculator
+//
+//  Created by Larry Nickerson on 11/29/17.
+//  Copyright © 2017 Larry Nickerson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
